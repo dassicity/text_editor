ff880660e36520d7aab1a95b125845a612c45ca1
Soumyanil Das <dassic@outlook.com>
2022-06-22 17:54:58 +0530

    Few changes

--- lico.c
+++ lico.c
@@ -1,6 +1,9 @@
+#include <unistd.h>
 #include <stdio.h>
 int main()
 {
-    printf("Hello, Namaskaram world\n");
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+        ;
     return 0;
 }
