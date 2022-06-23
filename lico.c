f0b1c743b87bfc9faaa0f3b5f569a62aad69bf0b
Soumyanil Das <dassic@outlook.com>
2022-06-23 22:15:07 +0530

    Added disableRawMode method

--- lico.c
+++ lico.c
@@ -1,16 +1,20 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdio.h>
+#include <stdlib.h>
 // #include<string.h>
+struct termios original_termios;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+}
 
 void enableRawMode(){
+    tcgetattr(STDIN_FILENO, &original_termios);
+    atexit(disableRawMode);
 
-    struct termios raw;
-
-    tcgetattr(STDIN_FILENO, &raw);
-
+    struct termios raw = original_termios;
     raw.c_lflag &= ~(ECHO);
-
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 
 
 }
