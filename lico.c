92448a3938b6d7ed0e980055b85c848a6f45eb79
Soumyanil Das <dassic@outlook.com>
2022-07-04 02:07:44 +0530

    Added timeout for read()

--- lico.c
+++ lico.c
@@ -8,37 +8,46 @@
 // #include<string.h>
 struct termios original_termios;
 
-void disableRawMode(){
+void disableRawMode()
+{
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
 }
 
-void enableRawMode(){
+void enableRawMode()
+{
     tcgetattr(STDIN_FILENO, &original_termios);
     atexit(disableRawMode);
 
     struct termios raw = original_termios;
-    raw.c_iflag &= ~(IXON | ICRNL);
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
     raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main()
 {
     enableRawMode();
-    char c;
-    // char text[50] = "";
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
-        if ( iscntrl(c)){
+    while (1)
+    {
+        char c = '\0';
+        // char text[50] = "";
+        read(STDIN_FILENO, &c, 1);
+
+        if (iscntrl(c))
+        {
             printf("%d\r\n", c);
         }
-        else {
+        else
+        {
             printf("%d ('%c)\r\n", c, c);
         }
-        //  strncat(text, &c, 1);
+        if (c == 'q')
+            break;
     }
-    // printf(text, '\n');
-    
     return 0;
 }
