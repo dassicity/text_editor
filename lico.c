781f7e5d443a124f54c4d01bdf51f8502b511a28
Soumyanil Das <dassic@outlook.com>
2022-07-11 23:40:45 +0530

    Added Append buffer method

--- lico.c
+++ lico.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -133,6 +134,23 @@ struct abuf
     {             \
         NULL, 0   \
     }
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL)
+        return;
+
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+    free(ab->b);
+}
 
 // ------ output ------
 
